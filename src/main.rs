00decfdb756b5663f685877a13f3f92ccce928c9
Axel Aparicio Rotermund <axelaparicior@gmail.com>
2025-04-22 03:13:57 -0300

    Added built-in exit and args to commands

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,25 @@ fn main() {
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
 
-        let command = input.trim();
+        let mut input_split = input.split_whitespace();
 
-        match Command::new(command).spawn() {
+        let command = input_split.next().unwrap_or("");
+        let args = input_split;
+
+        if command == "exit" {
+            return;
+        }
+
+        match Command::new(command).args(args).spawn() {
             Ok(mut child) => match child.wait() {
                 Ok(exit_status) => println!(
-                    "\n    Run [{:?}] exit_status: {:?}",
-                    command,
+                    "\n    \x1b[36mRun [{:?}] exit_status: {:?}\x1b[0m",
+                    input.trim(),
                     exit_status.code().unwrap_or(-1)
                 ),
-                Err(err) => println!("Error on wait: {:?}", err.to_string()),
+                Err(err) => println!("\x1b[31mError on wait: {:?}\x1b[0m", err.to_string()),
             },
-            Err(err) => println!("Error: {:?}", err.to_string()),
+            Err(err) => println!("\x1b[31mError: {:?}\x1b[0m", err.to_string()),
         }
     }
 }
