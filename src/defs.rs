c44da11ac4ab304fd7395ef6dc7953e3d96d58e5
Axel Aparicio Rotermund <axelaparicior@gmail.com>
2025-04-22 11:26:14 -0300

    Refactor colors

---
+++ src/defs.rs
@@ -0,0 +1,6 @@
+#![allow(dead_code)]
+
+pub const RED: &str = "\x1b[31m";
+pub const MAG: &str = "\x1b[35m";
+pub const BLU: &str = "\x1b[36m";
+pub const RESET: &str = "\x1b[0m";
--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,23 @@ use std::io::{self, stdout, Write};
 use std::path::{Path, PathBuf};
 use std::process::Command;
 
+mod defs;
+
 fn run_cmd(command: &str, args: Vec<&str>, input: String) {
     let child_cmd = Command::new(command).args(args).spawn();
 
     match child_cmd {
         Ok(mut child) => match child.wait() {
             Ok(exit_status) => println!(
-                "\n    \x1b[36mRun [{:?}] exit_status: {:?}\x1b[0m",
+                "\n    {}Run [{:?}] exit_status: {:?}{}",
+                defs::BLU,
                 input.trim(),
-                exit_status.code().unwrap_or(-1)
+                exit_status.code().unwrap_or(-1),
+                defs::RESET
             ),
-            Err(err) => eprintln!("\x1b[31mError on wait: {}\x1b[0m", err),
+            Err(err) => eprintln!("{}Error on wait: {}{}", defs::RED, err, defs::RESET),
         },
-        Err(err) => eprintln!("\x1b[31mError: {}\x1b[0m", err),
+        Err(err) => eprintln!("{}Error: {}{}", defs::RED, err, defs::RESET),
     }
 }
 
@@ -29,23 +33,27 @@ fn change_dir(dir: Vec<&str>, home: PathBuf) {
     };
 
     if let Err(err) = env::set_current_dir(root) {
-        eprintln!("{}", err);
+        eprintln!("{}Error: {}{}", defs::RED, err, defs::RESET);
     }
 }
 
 fn main() {
     let home = dirs::home_dir().unwrap_or(PathBuf::from("/"));
     if let Err(err) = env::set_current_dir(&home) {
-        eprintln!("{}", err)
+        eprintln!("{}Error: {}{}", defs::RED, err, defs::RESET);
     }
     loop {
         let current_dir = env::current_dir().unwrap();
-        println!("\x1b[35m({})\x1b[0m", current_dir.display());
+        println!("{}({}){}", defs::MAG, current_dir.display(), defs::RESET);
         print!(">> ");
         _ = stdout().flush();
 
         let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
+        let bytes_read = io::stdin().read_line(&mut input).unwrap();
+
+        if bytes_read == 0 {
+            return;
+        }
 
         let mut input_split = input.split_whitespace();
 
